af11f5c0d0ed8398f0b09ee5268143903b285be1
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-11-09 21:33:17 +0200

    initial styles

--- aleannlab-task/src/App.tsx
+++ aleannlab-task/src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-
-import './/css/App.css';
+import './css/page.css';
 
 function App() {
-  return (
-    <div className="wrapper">
-      <div>test</div>
-    </div>
-  );
+	return (
+		<div className="wrapper">
+			<div className='page'>
+				<div> test header</div>
+				<main className='main'>main</main>
+				<div>test footer</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
