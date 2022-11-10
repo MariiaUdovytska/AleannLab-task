aec28c233dfaf09d275f2d6025fd5a8ab09e27ca
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-11-10 18:19:45 +0200

    create component Stars

---
+++ aleannlab-task/src/stars/Stars.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../css/jobList/jobListCards.css';
+
+function Stars(props) {
+	return (
+		<div className='cardd__icons-stars'>
+			<i className="bi bi-star-fill"></i>
+			<i className="bi bi-star-fill"></i>
+			<i className="bi bi-star-fill"></i>
+			<i className="bi bi-star-fill"></i>
+			<i className="bi bi-star-fill"></i>
+		</div>
+	)
+}
+
+export default Stars;
