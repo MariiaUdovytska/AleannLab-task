4cec425d7254667c9b9701b0fa56d73bac6ad029
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-11-13 16:26:24 +0200

    correct render part from JobDetailedDescription

--- aleannlab-task/src/jobDetailed/JobDetailedDescription.js
+++ aleannlab-task/src/jobDetailed/JobDetailedDescription.js
@@ -7,15 +7,27 @@ function JobDetailedDescription(props) {
 	let first = data[0];
 	data = data[1].split('\nCompensation & Benefits:\n\t');
 	let responsopilities = data[0];
-	let compensation = data[1];
+	let compensation = data[1].split('.')
 
+	let compensationLi = [];
+	for (let index = 0; index < compensation.length; index++) {
+		const element = compensation[index];
+		if (element !== "\n\n") {
+			compensationLi.push(
+				<li key={index}>{element}</li>
+			)
+		}
+
+	}
 	return (
 		<div className='description'>
 			<p>{first}</p>
 			<h3>Responsopilities:</h3>
 			<p>{responsopilities}</p>
 			<h3>Compensation & Benefits:</h3>
-			<p>{compensation}</p>
+			<ul>
+				{compensationLi}
+			</ul>
 		</div>
 	)
 }
