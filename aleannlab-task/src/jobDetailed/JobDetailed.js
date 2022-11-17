63a452678813fca74547777dde5d3ed396a096ae
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-11-17 14:26:53 +0200

    correct JobDetailed comp to function on hooks, accept id for open page detailed

--- aleannlab-task/src/jobDetailed/JobDetailed.js
+++ aleannlab-task/src/jobDetailed/JobDetailed.js
@@ -1,57 +1,49 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
 import '../css/jobDetailed/jobDetailed.css';
 import dataResponse from '../data/dataResponse.json';
 import DataCalculator from '../dataCalculator/DataCalculator';
 import JobDetailedAdditional from './JobDetailedAdditional';
 import JobDetailedDescription from './JobDetailedDescription';
+// import Map from '../coordinates/Map';
 
 var isDebug = false;
 
-class JobDetailed extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			error: null,
-			isLoaded: false,
-			sizeWidthType: 'desktop',
-			jobs: []
+function JobDetailed(props) {
+	const [sizeWidthType, setSizeWidthType] = useState('desktop');
+	useEffect(() => {
+		let getWindowDimensions = () => {
+			const { innerWidth: width } = window;
+			return width;
+		}
+
+		let handleWindowDimensions = () => {
+			let width = getWindowDimensions()
+			if (width <= 626)
+				setSizeWidthType('tablet');
+			else
+				setSizeWidthType('desktop');
+		}
+
+		// this.loadData();
+		handleWindowDimensions();
+		window.addEventListener('resize', handleWindowDimensions);
+		// Указываем, как сбросить этот эффект:
+		return function cleanup() {
+			window.removeEventListener('resize', handleWindowDimensions);
 		};
-	}
+	}, []);
 
-	getWindowDimensions = () => {
-		const { innerWidth: width } = window;
-		return width;
-	}
-
-	handleWindowDimensions = () => {
-		let width = this.getWindowDimensions()
-		if (width <= 626)
-			this.setState({ sizeWidthType: 'tablet' });
-		else
-			this.setState({ sizeWidthType: 'desktop' });
-	}
-
-	componentDidMount() {
-		this.loadData();
-		this.handleWindowDimensions();
-		window.addEventListener('resize', this.handleWindowDimensions);
-	}
-
-	componentWillUnmount() {
-		window.removeEventListener('resize', this.handleWindowDimensions);
-	}
-
-	mapData = (data) => {
-		this.setState({
-			isLoaded: true,
-			jobs: data,
-		});
-	}
-	loadData = () => {
-
+	const [jobs, setJobs] = useState([]);
+	const [error, setError] = useState(null);
+	const [isLoaded, setIsLoaded] = useState(false);
+	useEffect(() => {
+		let mapData = (data) => {
+			setIsLoaded(true);
+			setJobs(data);
+		}
 		if (isDebug) {
-			this.mapData(dataResponse);
+			mapData(dataResponse);
 			return;
 		}
 
@@ -68,78 +60,78 @@ class JobDetailed extends React.Component {
 				if (response.ok) {
 					return response
 						.json()
-						.then(this.mapData);
+						.then(mapData);
 				}
 				throw new Error(`ничего не найдено`);
 			})
 			.catch(err => {
-				this.setState({
-					isLoaded: true,
-					error: err
-				});
+				setIsLoaded(true);
+				setError(err);
 			});
-	}
+	}, []);
 
-	render() {
-		const { error, isLoaded, jobs } = this.state;
+	const [searchParams] = useSearchParams();
+	let id = searchParams.get('id');
 
-		if (error) {
-			return <p> Error {error.message}</p>
-		} else if (!isLoaded) {
-			return <p> Loading...</p>
-		} else {
-			const job = jobs[0];
-			return (
-				<div className='job-detailed container-detailed'>
-					<div className='job-detailed__body'>
-						<div className='job-detailed__body-header'>
-							<h1>Job Details</h1>
-							<div className='job-detailed__body-header-icons'>
-								<div className='job-detailed__body-header-bookmark'>
-									<i className="bi bi-bookmark"></i>
-									<i className="bi bi-bookmark-fill"></i>
-									<span>Save to my list</span>
-								</div>
-								<div className='job-detailed__body-header-share'>
-									<i className="bi bi-share-fill"></i>
-									<span>Share</span>
-								</div>
+	if (error) {
+		return <p> Error {error.message}</p>
+	} else if (!isLoaded) {
+		return <p> Loading...</p>
+	} else {
+		const job = jobs.filter(job => job.id === id)[0];
+		if (job === undefined)
+			return (<>Wwong Id:{id}</>)
+
+		return (
+			<div className='job-detailed container-detailed'>
+				<div className='job-detailed__body'>
+					<div className='job-detailed__body-header'>
+						<h1>Job Details</h1>
+						<div className='job-detailed__body-header-icons'>
+							<div className='job-detailed__body-header-bookmark'>
+								<i className="bi bi-bookmark"></i>
+								<i className="bi bi-bookmark-fill"></i>
+								<span>Save to my list</span>
 							</div>
-						</div>
-						<div className='job-detailed__body-unbtn'>
-							<button type='button'>Apply now</button>
-						</div>
-						<div className='job-detailed__body-main'>
-							<div className='job-detailed__body-main-title'>
-								<h2>{job.title}</h2>
-								<div className='job-detailed__body-main-salary'>
-									€ {job.salary}
-									<span>Brutto, per year</span>
-								</div>
-								<DataCalculator updatedAt={job.updatedAt} class={true} />
+							<div className='job-detailed__body-header-share'>
+								<i className="bi bi-share-fill"></i>
+								<span>Share</span>
 							</div>
-							<JobDetailedDescription description={job.description} />
-						</div>
-						<div className='job-detailed__body-downbtn'>
-							<button type='button'>Apply now</button>
-						</div>
-						<JobDetailedAdditional additional={job} sizeWidthType={this.state.sizeWidthType} />
-						<div className='job-detailed__body-return'>
-							<Link to="/jobList">
-								<button type='button' >
-									<i className="bi bi-chevron-left"></i>
-									RETURN TO JOB BOARD
-								</button>
-							</Link>
 						</div>
 					</div>
-					<div className='job-detailed__map'>
-						<h4>Contacts</h4>
-						m ea enim consectetur sit magna minim ea cupidatat. Et ut proident voluptate quis nulla anim commodo in pariat
+					<div className='job-detailed__body-unbtn'>
+						<button type='button'>Apply now</button>
+					</div>
+					<div className='job-detailed__body-main'>
+						<div className='job-detailed__body-main-title'>
+							<h2>{job.title}</h2>
+							<div className='job-detailed__body-main-salary'>
+								€ {job.salary}
+								<span>Brutto, per year</span>
+							</div>
+							<DataCalculator updatedAt={job.updatedAt} class={true} />
+						</div>
+						<JobDetailedDescription description={job.description} />
+					</div>
+					<div className='job-detailed__body-downbtn'>
+						<button type='button'>Apply now</button>
+					</div>
+					<JobDetailedAdditional additional={job} sizeWidthType={sizeWidthType} />
+					<div className='job-detailed__body-return'>
+						<Link to="/jobList">
+							<button type='button' >
+								<i className="bi bi-chevron-left"></i>
+								RETURN TO JOB BOARD
+							</button>
+						</Link>
 					</div>
 				</div>
-			)
-		}
+				<div className='job-detailed__map'>
+					<h4>Contacts</h4>
+					{/* <Map /> */}
+				</div>
+			</div>
+		)
 	}
 }
 
